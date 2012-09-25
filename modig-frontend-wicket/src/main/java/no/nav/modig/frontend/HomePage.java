34846022266fa8849a5ab3535206e4cb1fcc1b89
Brødreskift, Kjell Arne <Kjell.Arne.Brodreskift@nav.no>
2012-09-25 07:30:31 +0000

    Flytter frontendkode tilbake til modig-frontend-wicket

--- modig-frontend-wicket/src/main/java/no/nav/modig/frontend/HomePage.java
+++ modig-frontend-wicket/src/main/java/no/nav/modig/frontend/HomePage.java
@@ -3,11 +3,14 @@ package no.nav.modig.frontend;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.resource.CssResourceReference;
+import org.apache.wicket.request.resource.JavaScriptResourceReference;
 
 
 public class HomePage extends WebPage {
 
     private static final long serialVersionUID = 1L;
+
 
     public HomePage(final PageParameters parameters) {
         super(parameters);
--- modig-frontend-wicket/src/main/java/no/nav/modig/frontend/WicketApplication.java
+++ modig-frontend-wicket/src/main/java/no/nav/modig/frontend/WicketApplication.java
@@ -1,9 +1,14 @@
 package no.nav.modig.frontend;
 
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.request.resource.CssResourceReference;
+import org.apache.wicket.request.resource.JavaScriptResourceReference;
 
 
 public class WicketApplication extends WebApplication {
+
+    public static final CssResourceReference CSS_RESOURCE = new CssResourceReference(WicketApplication.class, "modig.css");
+    public static final JavaScriptResourceReference JS_RESOURCE = new JavaScriptResourceReference(WicketApplication.class, "modig.js");
 
     @Override
     public Class<HomePage> getHomePage() {
@@ -12,6 +17,10 @@ public class WicketApplication extends WebApplication {
 
     @Override
     public void init() {
+
         super.init();
+
+        mountResource("/statisk/js/modig.css", CSS_RESOURCE);
+        mountResource("/statisk/css/modig.css", JS_RESOURCE);
     }
 }
