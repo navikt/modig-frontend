c867ce594003a483f51ece58bed79eb20078691e
Brødreskift, Kjell Arne <Kjell.Arne.Brodreskift@nav.no>
2012-10-02 11:20:10 +0000

    Frontend konfigurering

--- modig-frontend-wicket/src/main/java/no/nav/modig/frontend/FrontendConfigurator.java
+++ modig-frontend-wicket/src/main/java/no/nav/modig/frontend/FrontendConfigurator.java
@@ -5,24 +5,40 @@ import org.apache.wicket.protocol.http.WebApplication;
 
 public class FrontendConfigurator {
 
-    private JquerySource jquerySource;
+    private JquerySource jquerySource = JquerySource.FRONTEND;
+    private String basePath = "/statisk";
+
 
     public enum JquerySource {
         WICKET, FRONTEND, NONE
     }
 
-    public void configure(WebApplication application) {
 
-        configureJquery(application);
-
-        application.mountResource("/statisk/css/modig.css", FrontendResources.CSS_RESOURCES);
-        application.mountResource("/statisk/js/modig.js", FrontendResources.JS_RESOURCES);
-
+    public FrontendConfigurator withJquerySource(JquerySource jquerySource) {
+        this.jquerySource = jquerySource;
+        return this;
     }
 
+
+    public FrontendConfigurator withBasePath(String basePath) {
+        this.basePath = basePath;
+        return this;
+    }
+
+
+    public void configure(WebApplication application) {
+        configureJquery(application);
+        application.mountResource(basePath + "/css/modig.css", FrontendResources.CSS_RESOURCES);
+        application.mountResource(basePath + "/js/modig.js", FrontendResources.JS_RESOURCES);
+    }
+
+
     private void configureJquery(WebApplication application) {
+
         switch (jquerySource) {
             case FRONTEND:
+                application.getJavaScriptLibrarySettings().setJQueryReference(FrontendResources.JQUERY_RESOURCE);
+                application.mountResource(basePath + "/js/jquery/jquery.js", FrontendResources.JQUERY_RESOURCE);
                 break;
             case NONE:
                 application.getJavaScriptLibrarySettings().setJQueryReference(null);
--- modig-frontend-wicket/src/main/java/no/nav/modig/frontend/FrontendResources.java
+++ modig-frontend-wicket/src/main/java/no/nav/modig/frontend/FrontendResources.java
@@ -7,4 +7,5 @@ import org.apache.wicket.request.resource.JavaScriptResourceReference;
 public class FrontendResources {
     public static final CssResourceReference CSS_RESOURCES = new CssResourceReference(FrontendResources.class, "css/modig.css");
     public static final JavaScriptResourceReference JS_RESOURCES = new JavaScriptResourceReference(FrontendResources.class, "js/modig.js");
+    public static final JavaScriptResourceReference JQUERY_RESOURCE = new JavaScriptResourceReference(FrontendResources.class, "js/jquery/jquery-1.8.2.js");
 }
