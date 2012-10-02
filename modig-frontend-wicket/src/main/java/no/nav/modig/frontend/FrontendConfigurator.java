4361fc556501435f0bc383d60660b8a43bfcde7c
Brødreskift, Kjell Arne <Kjell.Arne.Brodreskift@nav.no>
2012-10-02 12:38:08 +0000

    Frontend konfigurering

--- modig-frontend-wicket/src/main/java/no/nav/modig/frontend/FrontendConfigurator.java
+++ modig-frontend-wicket/src/main/java/no/nav/modig/frontend/FrontendConfigurator.java
@@ -1,12 +1,19 @@
 package no.nav.modig.frontend;
 
+import org.apache.wicket.markup.head.CssReferenceHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
+import org.apache.wicket.markup.head.JavaScriptReferenceHeaderItem;
+import org.apache.wicket.markup.html.IHeaderContributor;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.request.resource.JavaScriptResourceReference;
 
 
 public class FrontendConfigurator {
 
     private JquerySource jquerySource = JquerySource.FRONTEND;
     private String basePath = "/statisk";
+    private JavaScriptResourceReference[] javaScriptResourceReferences = FrontendResources.JS_RESOURCES_ALL;
 
 
     public enum JquerySource {
@@ -26,10 +33,40 @@ public class FrontendConfigurator {
     }
 
 
+    public FrontendConfigurator withJavascriptResources(JavaScriptResourceReference... resources) {
+        this.javaScriptResourceReferences = resources;
+        return this;
+    }
+
+
     public void configure(WebApplication application) {
         configureJquery(application);
+        configureCss(application);
+        configureJavascript(application);
+    }
+
+
+    private void configureCss(WebApplication application) {
         application.mountResource(basePath + "/css/modig.css", FrontendResources.CSS_RESOURCES);
-        application.mountResource(basePath + "/js/modig.js", FrontendResources.JS_RESOURCES);
+        application.getHeaderContributorListenerCollection().add(new IHeaderContributor() {
+            @Override
+            public void renderHead(IHeaderResponse response) {
+                response.render(CssReferenceHeaderItem.forReference(FrontendResources.CSS_RESOURCES));
+            }
+        });
+    }
+
+
+    private void configureJavascript(WebApplication application) {
+        for (final JavaScriptResourceReference javaScriptResourceReference : javaScriptResourceReferences) {
+            application.mountResource(basePath + "/js/" + javaScriptResourceReference.getName(), javaScriptResourceReference);
+            application.getHeaderContributorListenerCollection().add(new IHeaderContributor() {
+                @Override
+                public void renderHead(IHeaderResponse response) {
+                    response.render(JavaScriptReferenceHeaderItem.forReference(javaScriptResourceReference));
+                }
+            });
+        }
     }
 
 
--- modig-frontend-wicket/src/main/java/no/nav/modig/frontend/FrontendResources.java
+++ modig-frontend-wicket/src/main/java/no/nav/modig/frontend/FrontendResources.java
@@ -6,6 +6,20 @@ import org.apache.wicket.request.resource.JavaScriptResourceReference;
 
 public class FrontendResources {
     public static final CssResourceReference CSS_RESOURCES = new CssResourceReference(FrontendResources.class, "css/modig.css");
-    public static final JavaScriptResourceReference JS_RESOURCES = new JavaScriptResourceReference(FrontendResources.class, "js/modig.js");
     public static final JavaScriptResourceReference JQUERY_RESOURCE = new JavaScriptResourceReference(FrontendResources.class, "js/jquery/jquery-1.8.2.js");
+
+    public static final JavaScriptResourceReference[] JS_RESOURCES_ALL = {new JavaScriptResourceReference(FrontendResources.class, "js/modig.js")};
+    public static final JavaScriptResourceReference JS_AFFIX = new JavaScriptResourceReference(FrontendResources.class, "js/bootstrap/bootstrap-affix.js");
+    public static final JavaScriptResourceReference JS_ALERT = new JavaScriptResourceReference(FrontendResources.class, "js/bootstrap/bootstrap-alert.js");
+    public static final JavaScriptResourceReference JS_BUTTON = new JavaScriptResourceReference(FrontendResources.class, "js/bootstrap/bootstrap-button.js");
+    public static final JavaScriptResourceReference JS_CAROUSEL = new JavaScriptResourceReference(FrontendResources.class, "js/bootstrap/bootstrap-carousel.js");
+    public static final JavaScriptResourceReference JS_COLLAPSE = new JavaScriptResourceReference(FrontendResources.class, "js/bootstrap/bootstrap-collapse.js");
+    public static final JavaScriptResourceReference JS_DROPDOWN = new JavaScriptResourceReference(FrontendResources.class, "js/bootstrap/bootstrap-dropdown.js");
+    public static final JavaScriptResourceReference JS_MODAL = new JavaScriptResourceReference(FrontendResources.class, "js/bootstrap/bootstrap-modal.js");
+    public static final JavaScriptResourceReference JS_POPOVER = new JavaScriptResourceReference(FrontendResources.class, "js/bootstrap/bootstrap-popover.js");
+    public static final JavaScriptResourceReference JS_SCROLLSPY = new JavaScriptResourceReference(FrontendResources.class, "js/bootstrap/bootstrap-scrollspy.js");
+    public static final JavaScriptResourceReference JS_TAB = new JavaScriptResourceReference(FrontendResources.class, "js/bootstrap/bootstrap-tab.js");
+    public static final JavaScriptResourceReference JS_TOOLTIP = new JavaScriptResourceReference(FrontendResources.class, "js/bootstrap/bootstrap-tooltip.js");
+    public static final JavaScriptResourceReference JS_TRANSITION = new JavaScriptResourceReference(FrontendResources.class, "js/bootstrap/bootstrap-transition.js");
+    public static final JavaScriptResourceReference JS_TYPEAHEAD = new JavaScriptResourceReference(FrontendResources.class, "js/bootstrap/bootstrap-typeahead.js");
 }
