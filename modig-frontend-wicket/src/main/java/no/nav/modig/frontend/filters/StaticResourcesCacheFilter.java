ab66b999afcfb5cb6e7a9c092bc1c02d03bf76a2
Andreas Baakind <andreas.baakind@nav.no>
2014-01-21 14:00:20 +0100

    PK-6016 Implementert cachefilter i 1.0 branchen

---
+++ modig-frontend-wicket/src/main/java/no/nav/modig/frontend/filters/StaticResourcesCacheFilter.java
@@ -0,0 +1,48 @@
+package no.nav.modig.frontend.filters;
+
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.joda.time.format.DateTimeFormat.forPattern;
+
+public class StaticResourcesCacheFilter implements Filter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(StaticResourcesCacheFilter.class);
+    // Formatet: 'Tue, 15 Nov 1994 08:12:31 GMT'
+    private static final String PATTERN = "EE, dd MMM yyyy HH:mm:ss zz";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        LOG.debug("Cachefilter init");
+    }
+
+    @Override
+    public void doFilter(final ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        LOG.debug("Cachefilter doFilter");
+        HttpServletResponse httpResponse = (HttpServletResponse)response;
+
+        httpResponse.addHeader("Cache-control", "public, max-age=600");
+        httpResponse.addHeader("Expires", convertToHeaderDate(DateTime.now().plusMinutes(10)));
+
+        chain.doFilter(request, response);
+    }
+
+    private String convertToHeaderDate(DateTime dateTime) {
+        return forPattern(PATTERN).print(dateTime);
+    }
+
+    @Override
+    public void destroy() {
+        LOG.debug("Cachefilter destroy");
+    }
+}
