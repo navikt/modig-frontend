4f656d6155fe389f6d726476e549d3efb0d2add9
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2014-02-27 12:16:14 +0100

    Fjernet foldere med original bootstrap-filer. Korrigert js byggescript til å bygge komplette moduler for hhv bootstrap og modig.

--- modig-frontend-ressurser/src/main/nodejs/build-frontend.js
+++ modig-frontend-ressurser/src/main/nodejs/build-frontend.js
@@ -1,144 +1,195 @@
 var args = process.argv;
 var argsLength = args.length;
 if (argsLength < 3) {
-    var hash = '\n##################################################\n';
-    console.log('\nFor få argumenter:' + hash + 'Riktig bruk er: node build-bootstrap.js src-katalog' + hash); }
+	var hash = '\n##################################################\n';
+	console.log('\nFor få argumenter:' + hash + 'Riktig bruk er: node build-bootstrap.js src-katalog' + hash);
+}
 console.log('Arguments: ', args);
 
 var baseFolder = args[2];
+var resourceFolder = baseFolder + "/src/main/resources";
+var generateFolder = baseFolder + "/src/main/generated-resources";
 
 var fs = require('fs');
 var util = require('util');
 var remove = require('remove');
-
 var ugly = require('uglifyjs');
-
 var less = require('less');
+
 
 
 // SLETT OG GJENOPPRETT GENERATED-RESOURCES
 clean();
 
-// KOPIER BOOTSTRAP JAVASCRIPT-FILER
-copy(baseFolder + '/src/main/bootstrap/js/', baseFolder + '/src/main/generated-resources/js/bootstrap/');
 
-// KOPIER BOOTSTRAP LESS-FILER
-copy(baseFolder + '/src/main/bootstrap/less/', baseFolder + '/src/main/generated-resources/less/bootstrap/');
+// KOPIER JAVASCRIPT-FILER
+copy(resourceFolder + '/js/bootstrap/', generateFolder + '/js/bootstrap/');
+copy(resourceFolder + '/js/felles/', generateFolder + '/js/modig/felles/');
+copy(resourceFolder + '/js/jquery/', generateFolder + '/js/modig/jquery/');
+copy(resourceFolder + '/js/jquery/', generateFolder + '/js/bootstrap/jquery/');
 
-// KOPIER BOOTSTRAP BILDER
-copy(baseFolder + '/src/main/bootstrap/img/', baseFolder + '/src/main/generated-resources/img/');
+copyFile(resourceFolder + '/js/', generateFolder + '/js/bootstrap/', 'console.js');
+copyFile(resourceFolder + '/js/', generateFolder + '/js/bootstrap/', 'html5.js');
+copyFile(resourceFolder + '/js/', generateFolder + '/js/bootstrap/', 'html5.min.js');
+copyFile(resourceFolder + '/js/', generateFolder + '/js/bootstrap/', 'underscore.js');
+copyFile(resourceFolder + '/js/', generateFolder + '/js/bootstrap/', 'underscore-min.js');
 
-// SLÅ SAMMEN BOOTSTRAP JAVASCRIPT-FILER
-concat(baseFolder + '/src/main/bootstrap/js/', baseFolder + '/src/main/generated-resources/js/modig.js');
-
-// MINIFIERE BOOTSTRAP JAVASCRIPT-FILER ENKELTVIS
-uglifySingles(baseFolder + '/src/main/bootstrap/js/', baseFolder + '/src/main/generated-resources/js/bootstrap/');
-
-// MINIFIERE SAMMENSLÅTT JAVASCRIPT-FIL
-uglifyConcatenated(baseFolder + '/src/main/generated-resources/js/modig.js', baseFolder + '/src/main/generated-resources/js/modig.min.js');
-
-// SLÅ SAMMEN OG MINIFIERE CSS-FILER
-runLess(baseFolder + '/src/main/bootstrap/less/', baseFolder + '/src/main/modig.less', baseFolder + '/src/main/generated-resources/css/modig.css');
+copyFile(resourceFolder + '/js/', generateFolder + '/js/modig/', 'console.js');
+copyFile(resourceFolder + '/js/', generateFolder + '/js/modig/', 'html5.js');
+copyFile(resourceFolder + '/js/', generateFolder + '/js/modig/', 'html5.min.js');
+copyFile(resourceFolder + '/js/', generateFolder + '/js/modig/', 'underscore.js');
+copyFile(resourceFolder + '/js/', generateFolder + '/js/modig/', 'underscore-min.js');
 
 
-// VENTER MED Å BYGGE MODULARISERT CSS TIL DETTE ER STØTTET I BOOTSTRAP
-//runLess(baseFolder + '/src/main/bootstrap/less/', baseFolder + '/src/main/common.less', baseFolder + '/src/main/generated-resources/css/bootstrap/common.css');
-//runLess(baseFolder + '/src/main/bootstrap/less/', baseFolder + '/src/main/alert.less', baseFolder + '/src/main/generated-resources/css/bootstrap/alert.css');
-//runLess(baseFolder + '/src/main/bootstrap/less/', baseFolder + '/src/main/button.less', baseFolder + '/src/main/generated-resources/css/bootstrap/button.css');
-//runLess(baseFolder + '/src/main/bootstrap/less/', baseFolder + '/src/main/carousel.less', baseFolder + '/src/main/generated-resources/css/bootstrap/carousel.css');
-//runLess(baseFolder + '/src/main/bootstrap/less/', baseFolder + '/src/main/dropdown.less', baseFolder + '/src/main/generated-resources/css/bootstrap/dropdown.css');
-//runLess(baseFolder + '/src/main/bootstrap/less/', baseFolder + '/src/main/modal.less', baseFolder + '/src/main/generated-resources/css/bootstrap/modal.css');
-//runLess(baseFolder + '/src/main/bootstrap/less/', baseFolder + '/src/main/popover.less', baseFolder + '/src/main/generated-resources/css/bootstrap/popover.css');
-//runLess(baseFolder + '/src/main/bootstrap/less/', baseFolder + '/src/main/tooltip.less', baseFolder + '/src/main/generated-resources/css/bootstrap/tooltip.css');
+// KOPIER LESS-FILER
+copy(resourceFolder + '/less/bootstrap/', generateFolder + '/less/bootstrap/');
+copy(resourceFolder + '/less/felles/', generateFolder + '/less/modig/felles/');
+copy(resourceFolder + '/less/ekstern/', generateFolder + '/less/modig/ekstern/');
+
+
+// KOPIER CSS-FILER
+copy(resourceFolder + '/css/', generateFolder + '/css/modig/');
+
+
+// KOPIER BILDER
+copy(resourceFolder + '/img/', generateFolder + '/img/');
+
+
+
+// BOOTSTRAP:
+
+// SLÅ SAMMEN JAVASCRIPTFILER
+var files = fs.readdirSync(resourceFolder + '/js/bootstrap/');
+concat(resourceFolder + '/js/bootstrap/', generateFolder + '/js/bootstrap/bootstrap.js', files);
+
+// MINIFIERE JAVASCRIPTFIL
+uglifyConcatenated(generateFolder + '/js/bootstrap/bootstrap.js', generateFolder + '/js/bootstrap/bootstrap.min.js');
+
+// SLÅ SAMMEN OG MINIFIERE LESS-FILER
+runLess(resourceFolder + '/less/bootstrap/', resourceFolder + '/less/bootstrap/bootstrap.less', generateFolder + '/css/bootstrap/bootstrap.css');
+
+
+
+// MODIG:
+
+// SLÅ SAMMEN JAVASCRIPTFILER
+files = ['transitions.js', 'felles.js', 'dropdown.js', 'tooltip.js', 'AjaxLoader.js'];
+concat(resourceFolder + '/js/felles/', generateFolder + '/js/modig/modig.js', files);
+
+// MINIFIERE JAVASCRIPTFIL
+uglifyConcatenated(generateFolder + '/js/modig/modig.js', generateFolder + '/js/modig/modig.min.js');
+
+// SLÅ SAMMEN OG MINIFIERE LESS-FILER
+files = ['reset.less', 'variables.less', 'mixins.less', 'modus.less', 'typografi.less', 'felles.less', 'knapper.less',
+	'contrast.less', 'dropdowns.less', 'skjema.less', 'tooltip.less', 'utilities.less'];
+concat(resourceFolder + '/less/felles/', generateFolder + '/less/modig/felles.less', files);
+
+concat(resourceFolder + '/less/ekstern/', generateFolder + '/less/modig/ekstern.less', ['panel.less', 'footer.less', 'ekstern.less']);
+concat(generateFolder + '/less/modig/', generateFolder + '/less/modig/modig.less', ['felles.less', 'ekstern.less']);
+
+remove.removeSync(generateFolder + '/less/modig/felles.less');
+remove.removeSync(generateFolder + '/less/modig/ekstern.less');
+
+runLess(generateFolder + '/less/modig/', generateFolder + '/less/modig/modig.less', generateFolder + '/css/modig/modig.css');
+
 
 
 function clean() {
-    try {
-        if (fs.existsSync(baseFolder + '/src/main/generated-resources/')) {
-            remove.removeSync(baseFolder + '/src/main/generated-resources/');
-            console.log('Sletting av generated-resources OK!');
-        }
-    } catch (err) {
-        throw(err);
-    }
-    fs.mkdirSync(baseFolder + '/src/main/generated-resources/');
-    fs.mkdirSync(baseFolder + '/src/main/generated-resources/css/');
-    fs.mkdirSync(baseFolder + '/src/main/generated-resources/css/bootstrap/');
-    fs.mkdirSync(baseFolder + '/src/main/generated-resources/img/');
-    fs.mkdirSync(baseFolder + '/src/main/generated-resources/js/');
-    fs.mkdirSync(baseFolder + '/src/main/generated-resources/js/bootstrap/');
-    fs.mkdirSync(baseFolder + '/src/main/generated-resources/less/');
-    fs.mkdirSync(baseFolder + '/src/main/generated-resources/less/bootstrap/');
+	var folder = generateFolder + '/';
+	try {
+		if (fs.existsSync(folder)) {
+			remove.removeSync(folder);
+			console.log('Sletting av generated-resources OK!');
+		}
+	} catch (err) {
+		throw(err);
+	}
+	fs.mkdirSync(folder);
+	fs.mkdirSync(folder + 'css/');
+	fs.mkdirSync(folder + 'css/bootstrap/');
+	fs.mkdirSync(folder + 'css/modig/');
+	fs.mkdirSync(folder + 'img/');
+	fs.mkdirSync(folder + 'js/');
+	fs.mkdirSync(folder + 'js/bootstrap/');
+	fs.mkdirSync(folder + 'js/bootstrap/jquery/');
+	fs.mkdirSync(folder + 'js/modig/');
+	fs.mkdirSync(folder + 'js/modig/felles/');
+	fs.mkdirSync(folder + 'js/modig/jquery/');
+	fs.mkdirSync(folder + 'less/');
+	fs.mkdirSync(folder + 'less/bootstrap/');
+	fs.mkdirSync(folder + 'less/modig/');
+	fs.mkdirSync(folder + 'less/modig/ekstern/');
+	fs.mkdirSync(folder + 'less/modig/felles/');
 }
 
 function copy(fromDir, toDir) {
-    console.log('Kopier filer fra ' + fromDir + ' til ' + toDir);
-    var files = fs.readdirSync(fromDir);
-    console.log(files.length + 'filer');
-    for (var i = 0; i < files.length; i++) {
-        var file = files[i];
-        if (fs.statSync(fromDir + file).isFile()) {
-            console.log('File: ' + i + ': ', file);
-            var fullfile = fs.readFileSync(fromDir + file);
-            fs.writeFileSync(toDir + file, fullfile);
-        }
-    }
+	var files = fs.readdirSync(fromDir);
+	for (var i = 0; i < files.length; i++) {
+		var file = files[i];
+		copyFile(fromDir, toDir, file);
+	}
 }
 
-function concat(fromDir, toFile) {
-    var files = ['bootstrap-transition.js', 'bootstrap-alert.js', 'bootstrap-button.js', 'bootstrap-carousel.js',
-        'bootstrap-collapse.js', 'bootstrap-dropdown.js', 'bootstrap-modal.js', 'bootstrap-tooltip.js',
-        'bootstrap-popover.js', 'bootstrap-scrollspy.js', 'bootstrap-tab.js', 'bootstrap-typeahead.js',
-        'bootstrap-affix.js'];
-    var fullfile = '';
-    for (var i = 0; i < files.length; i++) {
-        var file = files[i];
-        fullfile += fs.readFileSync(fromDir + file, 'utf8');
-    }
-    fs.appendFileSync(toFile, fullfile, encoding = 'utf8'); }
+function copyFile(fromDir, toDir, file) {
+	console.log('Kopier ' + fromDir + file + ' -> ' + toDir);
+	if (fs.statSync(fromDir + file).isFile()) {
+		var fullfile = fs.readFileSync(fromDir + file);
+		fs.writeFileSync(toDir + file, fullfile);
+	}
+}
+
+function concat(fromDir, toFile, files) {
+	var fullfile = '';
+	for (var i = 0; i < files.length; i++) {
+		var file = files[i];
+		fullfile += fs.readFileSync(fromDir + file, 'utf8');
+	}
+	fs.appendFileSync(toFile, fullfile, encoding = 'utf8');
+}
 
 function uglifySingles(fromDir, toDir) {
-    var files = fs.readdirSync(fromDir);
-    for (var i = 0; i < files.length; i++) {
-        var file = files[i];
-        var ind = file.indexOf('.js');
-        if (file.substring(0, 1) !== '.' && ind > -1) {
-            console.log('File: ' + i + ': ', file);
-            var code = fs.readFileSync(fromDir + file, 'utf8');
-            var finalCode = makeUgly(code) + ';';
-            fs.writeFileSync(toDir + file.substring(0, ind) + '.min.js', finalCode, 'utf8');
-        }
-    }
+	var files = fs.readdirSync(fromDir);
+	for (var i = 0; i < files.length; i++) {
+		var file = files[i];
+		var ind = file.indexOf('.js');
+		if (file.substring(0, 1) !== '.' && ind > -1) {
+			console.log('File: ' + i + ': ', file);
+			var code = fs.readFileSync(fromDir + file, 'utf8');
+			var finalCode = makeUgly(code) + ';';
+			fs.writeFileSync(toDir + file.substring(0, ind) + '.min.js', finalCode, 'utf8');
+		}
+	}
 }
 
 function makeUgly(code) {
-    return ugly.minify(code, {fromString:true, output:{comments:true}}).code;
+	return ugly.minify(code, {fromString: true, output: {comments: true}}).code;
 }
 
 function uglifyConcatenated(fromFile, toFile) {
-    var code = fs.readFileSync(fromFile, 'utf8');
-    var finalCode = makeUgly(code);
-    fs.writeFileSync(toFile, finalCode, 'utf8'); }
+	var code = fs.readFileSync(fromFile, 'utf8');
+	var finalCode = makeUgly(code);
+	fs.writeFileSync(toFile, finalCode, 'utf8');
+}
 
 function runLess(lessDir, lessFile, cssFile) {
-    console.log('Running less. Dir: ' + lessDir + ' lessFile: ' + lessFile + ' cssFile: ' + cssFile);
-    var code = fs.readFileSync(lessFile, 'utf8');
-    var parser = new (less.Parser)({
-        paths: [lessDir],
-        filename: lessFile
-    });
+	console.log('Running less. Dir: ' + lessDir + ' lessFile: ' + lessFile + ' cssFile: ' + cssFile);
+	var code = fs.readFileSync(lessFile, 'utf8');
+	var parser = new (less.Parser)({
+		paths   : [lessDir],
+		filename: lessFile
+	});
 
-    parser.parse(code, function (err, tree) {
-        if (err) {
-            console.error(err);
-        }
-        try {
-            var css = tree.toCSS({ compress: true, yuicompress: true });
-        } catch (e) {
-            console.error(util.inspect(e));
-            throw e;
-        }
-        fs.writeFileSync(cssFile, css, 'utf8');
-    });
+	parser.parse(code, function (err, tree) {
+		if (err) {
+			console.error(err);
+		}
+		try {
+			var css = tree.toCSS({ compress: true, yuicompress: true });
+		} catch (e) {
+			console.error(util.inspect(e));
+			throw e;
+		}
+		fs.writeFileSync(cssFile, css, 'utf8');
+	});
 }
 
