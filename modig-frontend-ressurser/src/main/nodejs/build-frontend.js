e4b0aa88ac65f6031496182212dd707cab2e64d8
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2014-03-05 12:14:16 +0100

    Refaktorert byggescript. Lagt data som filnavn og stier ut i en json konfigurasjonsfil.

--- modig-frontend-ressurser/src/main/nodejs/build-frontend.js
+++ modig-frontend-ressurser/src/main/nodejs/build-frontend.js
@@ -1,101 +1,30 @@
 var args = process.argv;
-var argsLength = args.length;
-if (argsLength < 3) {
+if (args.length < 3) {
 	var hash = '\n##################################################\n';
-	console.log('\nFor få argumenter:' + hash + 'Riktig bruk er: node build-bootstrap.js src-katalog' + hash);
+	console.log('\nFor få argumenter:' + hash + 'Riktig bruk er: node build-frontend.js src-katalog' + hash);
 }
 console.log('Arguments: ', args);
 
-var baseFolder = args[2];
-var resourceFolder = baseFolder + "/src/main/resources";
-var generateFolder = baseFolder + "/src/main/generated-resources";
+var baseFolder = args[2] + '/src/main/';
+var fs      = require('fs');
+var util    = require('util');
+var remove  = require('remove');
+var ugly    = require('uglifyjs');
+var less    = require('less');
+var config  = readConfigFile('/nodejs/build-frontend-config.json');
 
-var fs = require('fs');
-var util = require('util');
-var remove = require('remove');
-var ugly = require('uglifyjs');
-var less = require('less');
-
-
-
-// SLETT OG GJENOPPRETT GENERATED-RESOURCES
-clean();
-
-
-// KOPIER JAVASCRIPT-FILER
-copy(resourceFolder + '/js/bootstrap/', generateFolder + '/js/bootstrap/');
-copy(resourceFolder + '/js/felles/', generateFolder + '/js/modig/felles/');
-copy(resourceFolder + '/js/jquery/', generateFolder + '/js/modig/jquery/');
-copy(resourceFolder + '/js/jquery/', generateFolder + '/js/bootstrap/jquery/');
-
-copyFile(resourceFolder + '/js/', generateFolder + '/js/bootstrap/', 'console.js');
-copyFile(resourceFolder + '/js/', generateFolder + '/js/bootstrap/', 'html5.js');
-copyFile(resourceFolder + '/js/', generateFolder + '/js/bootstrap/', 'html5.min.js');
-copyFile(resourceFolder + '/js/', generateFolder + '/js/bootstrap/', 'underscore.js');
-copyFile(resourceFolder + '/js/', generateFolder + '/js/bootstrap/', 'underscore-min.js');
-
-copyFile(resourceFolder + '/js/', generateFolder + '/js/modig/', 'console.js');
-copyFile(resourceFolder + '/js/', generateFolder + '/js/modig/', 'html5.js');
-copyFile(resourceFolder + '/js/', generateFolder + '/js/modig/', 'html5.min.js');
-copyFile(resourceFolder + '/js/', generateFolder + '/js/modig/', 'underscore.js');
-copyFile(resourceFolder + '/js/', generateFolder + '/js/modig/', 'underscore-min.js');
-
-
-// KOPIER LESS-FILER
-copy(resourceFolder + '/less/bootstrap/', generateFolder + '/less/bootstrap/');
-copy(resourceFolder + '/less/felles/', generateFolder + '/less/modig/felles/');
-copy(resourceFolder + '/less/ekstern/', generateFolder + '/less/modig/ekstern/');
-
-
-// KOPIER CSS-FILER
-copy(resourceFolder + '/css/', generateFolder + '/css/modig/');
-
-
-// KOPIER BILDER
-copy(resourceFolder + '/img/', generateFolder + '/img/');
-
-
-
-// BOOTSTRAP:
-
-// SLÅ SAMMEN JAVASCRIPTFILER
-var files = fs.readdirSync(resourceFolder + '/js/bootstrap/');
-concat(resourceFolder + '/js/bootstrap/', generateFolder + '/js/bootstrap/bootstrap.js', files);
-
-// MINIFIERE JAVASCRIPTFIL
-uglifyConcatenated(generateFolder + '/js/bootstrap/bootstrap.js', generateFolder + '/js/bootstrap/bootstrap.min.js');
-
-// SLÅ SAMMEN OG MINIFIERE LESS-FILER
-runLess(resourceFolder + '/less/bootstrap/', resourceFolder + '/less/bootstrap/bootstrap.less', generateFolder + '/css/bootstrap/bootstrap.css');
-
-
-
-// MODIG:
-
-// SLÅ SAMMEN JAVASCRIPTFILER
-files = ['transitions.js', 'felles.js', 'dropdown.js', 'tooltip.js', 'AjaxLoader.js'];
-concat(resourceFolder + '/js/felles/', generateFolder + '/js/modig/modig.js', files);
-
-// MINIFIERE JAVASCRIPTFIL
-uglifyConcatenated(generateFolder + '/js/modig/modig.js', generateFolder + '/js/modig/modig.min.js');
-
-// SLÅ SAMMEN OG MINIFIERE LESS-FILER
-files = ['reset.less', 'variables.less', 'mixins.less', 'modus.less', 'typografi.less', 'felles.less', 'knapper.less',
-	'contrast.less', 'dropdowns.less', 'skjema.less', 'tooltip.less', 'utilities.less'];
-concat(resourceFolder + '/less/felles/', generateFolder + '/less/modig/felles.less', files);
-
-concat(resourceFolder + '/less/ekstern/', generateFolder + '/less/modig/ekstern.less', ['panel.less', 'footer.less', 'ekstern.less']);
-concat(generateFolder + '/less/modig/', generateFolder + '/less/modig/modig.less', ['felles.less', 'ekstern.less']);
-
-remove.removeSync(generateFolder + '/less/modig/felles.less');
-remove.removeSync(generateFolder + '/less/modig/ekstern.less');
-
-runLess(generateFolder + '/less/modig/', generateFolder + '/less/modig/modig.less', generateFolder + '/css/modig/modig.css');
-
+clean();        // SLETTE OG GJENOPPRETTE GENERATED-RESOURCES
+copyFolders();  // KOPIERE KATALOGER
+copyFiles();    // KOPIERE FILER
+concatFiles();  // SLÅ SAMMEN FILER
+uglifyFiles();  // MINIFIERE FILER
+removeFiles();  // SLETTE FILER
+runLessFiles(); // SLÅ SAMMEN OG MINIFIERE LESS-FILER
 
 
 function clean() {
-	var folder = generateFolder + '/';
+	var files = config.fileStructure;
+	var folder = baseFolder + '/generated-resources/';
 	try {
 		if (fs.existsSync(folder)) {
 			remove.removeSync(folder);
@@ -104,29 +33,33 @@ function clean() {
 	} catch (err) {
 		throw(err);
 	}
-	fs.mkdirSync(folder);
-	fs.mkdirSync(folder + 'css/');
-	fs.mkdirSync(folder + 'css/bootstrap/');
-	fs.mkdirSync(folder + 'css/modig/');
-	fs.mkdirSync(folder + 'img/');
-	fs.mkdirSync(folder + 'js/');
-	fs.mkdirSync(folder + 'js/bootstrap/');
-	fs.mkdirSync(folder + 'js/bootstrap/jquery/');
-	fs.mkdirSync(folder + 'js/modig/');
-	fs.mkdirSync(folder + 'js/modig/felles/');
-	fs.mkdirSync(folder + 'js/modig/jquery/');
-	fs.mkdirSync(folder + 'less/');
-	fs.mkdirSync(folder + 'less/bootstrap/');
-	fs.mkdirSync(folder + 'less/modig/');
-	fs.mkdirSync(folder + 'less/modig/ekstern/');
-	fs.mkdirSync(folder + 'less/modig/felles/');
+	for (var i = 0; i < files.length; i++) {
+		var file = files[i];
+		fs.mkdirSync(folder + file);
+	}
 }
 
-function copy(fromDir, toDir) {
+function copyFolders() {
+	var tasks = config.copyFolders;
+	for (var i = 0; i < tasks.length; i++) {
+		var task = tasks[i];
+		copyFolder(baseFolder + task.fromDir, baseFolder + task.toDir);
+	}
+}
+
+function copyFolder(fromDir, toDir) {
 	var files = fs.readdirSync(fromDir);
 	for (var i = 0; i < files.length; i++) {
 		var file = files[i];
 		copyFile(fromDir, toDir, file);
+	}
+}
+
+function copyFiles() {
+	var tasks = config.copyFiles;
+	for (var i = 0; i < tasks.length; i++) {
+		var task = tasks[i];
+		copyFile(baseFolder + task.fromDir, baseFolder + task.toDir, task.file);
 	}
 }
 
@@ -138,13 +71,32 @@ function copyFile(fromDir, toDir, file) {
 	}
 }
 
+function concatFiles() {
+	var tasks = config.concatFiles;
+	for (var i = 0; i < tasks.length; i++) {
+		var task = tasks[i];
+		concat(baseFolder + task.fromDir, baseFolder + task.toFile, task.files);
+	}
+}
+
 function concat(fromDir, toFile, files) {
+	if (files.indexOf('fs.readdirSync') === 0) {
+		files = eval(files);
+	}
 	var fullfile = '';
 	for (var i = 0; i < files.length; i++) {
 		var file = files[i];
 		fullfile += fs.readFileSync(fromDir + file, 'utf8');
 	}
 	fs.appendFileSync(toFile, fullfile, encoding = 'utf8');
+}
+
+function uglifyFiles() {
+	var tasks = config.uglify;
+	for (var i = 0; i < tasks.length; i++) {
+		var task = tasks[i];
+		uglifyConcatenated(baseFolder + task.fromFile, baseFolder + task.toFile);
+	}
 }
 
 function uglifySingles(fromDir, toDir) {
@@ -161,14 +113,29 @@ function uglifySingles(fromDir, toDir) {
 	}
 }
 
-function makeUgly(code) {
-	return ugly.minify(code, {fromString: true, output: {comments: true}}).code;
-}
-
 function uglifyConcatenated(fromFile, toFile) {
 	var code = fs.readFileSync(fromFile, 'utf8');
 	var finalCode = makeUgly(code);
 	fs.writeFileSync(toFile, finalCode, 'utf8');
+}
+
+function makeUgly(code) {
+	return ugly.minify(code, {fromString: true, output: {comments: true}}).code;
+}
+
+function removeFiles() {
+	var tasks = config.remove;
+	for (var i = 0; i < tasks.length; i++) {
+		remove.removeSync(baseFolder + tasks[i]);
+	}
+}
+
+function runLessFiles() {
+	var tasks = config.runLess;
+	for (var i = 0; i < tasks.length; i++) {
+		var task = tasks[i];
+		runLess(baseFolder + task.fromDir, baseFolder + task.mainFile, baseFolder + task.builtFile);
+	}
 }
 
 function runLess(lessDir, lessFile, cssFile) {
@@ -193,3 +160,17 @@ function runLess(lessDir, lessFile, cssFile) {
 	});
 }
 
+function readConfigFile(file) {
+	return JSON.parse(fs.readFileSync(
+		baseFolder + file,
+		'utf-8',
+		function (err, data) {
+			if (err) {
+				console.log('Error: ' + err);
+				return;
+			}
+			data = JSON.parse(data);
+			console.dir(data);
+		}
+	));
+}
