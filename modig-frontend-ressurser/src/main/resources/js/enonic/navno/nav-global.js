d93a3c77307a966f782016d013a76deb3e79e531
Anders Skifte <Anders.Skifte@nav.no>
2014-09-19 16:22:59 +0200

    Stiling av temavelger. Fiksa submit og validering.

--- modig-frontend-ressurser/src/main/resources/js/enonic/navno/nav-global.js
+++ modig-frontend-ressurser/src/main/resources/js/enonic/navno/nav-global.js
@@ -935,3 +935,28 @@ $(function () {
 });
 
 //////////////////////// END ////////////////////////
+
+$(function() {
+
+    $('#write-to-us').on('submit', function(e) {
+        e.preventDefault();
+        var errorMsg = $('#write-to-us-error');
+
+        errorMsg.addClass('hidden');
+
+        var checked = $('input:checked', this);
+
+        if(checked.length) {
+          var url = checked.data('url');
+          window.location = url;
+        }
+
+        else {
+           errorMsg.removeClass('hidden').focus();
+           return false;
+        }
+
+
+    });
+
+});
