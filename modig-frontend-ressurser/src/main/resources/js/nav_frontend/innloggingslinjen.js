c06e5bf364a7b390c6573f65554d0da6a4d174d5
Henrik Wingerei <henrik.wingerei@nav.no>
2015-04-13 13:17:42 +0200

    [PK-21908] Eksponerer sikkerhetsnivå som integer

--- modig-frontend-ressurser/src/main/resources/js/nav_frontend/innloggingslinjen.js
+++ modig-frontend-ressurser/src/main/resources/js/nav_frontend/innloggingslinjen.js
@@ -14,7 +14,7 @@ var Innloggingslinje = (function () {
 
             if (NAVStatusAJAX.readyState == 4 && NAVStatusAJAX.status == 200) {
                 var json = JSON.parse(NAVStatusAJAX.responseText);
-                navno.securityLevel = json.securityLevel;
+                navno.securityLevel = parseInt(json.securityLevel);
                 if (!json.authenticated) {
                     onNotAuthenticated();
                 } else if (json.securityLevel == 2) {
