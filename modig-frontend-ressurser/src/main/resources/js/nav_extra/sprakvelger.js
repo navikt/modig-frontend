d95fba1daf866fb581384d2c8cb90308cabafc6f
b123563 <kjell.arne.brodreskift@nav.no>
2015-06-17 14:57:33 +0200

    Endret struktur på less-filer

--- modig-frontend-ressurser/src/main/resources/js/nav_extra/sprakvelger.js
+++ modig-frontend-ressurser/src/main/resources/js/nav_extra/sprakvelger.js
@@ -1,6 +1,6 @@
 navno.onClickEnterContentLanguage = function (langList) {
 
-    var langClick = function (event) {
+    return function(event) {
 
         var langSelector = $('.content-languages');
 
@@ -8,21 +8,20 @@ navno.onClickEnterContentLanguage = function (langList) {
             event.stopPropagation();
 
             var globalLang = $("header.siteheader ul.dropdown-menu");
-            if (! globalLang.hasClass("hidden")) {
+            if (!globalLang.hasClass("hidden")) {
                 globalLang.addClass("hidden");
             }
         }
 
 
-        if (! langSelector.hasClass('selected')) {
+        if (!langSelector.hasClass('selected')) {
             langSelector.addClass('selected');
             langList.removeClass('hide');
         } else {
             langList.addClass('hide');
             langSelector.removeClass('selected');
         }
-    }
-    return langClick;
+    };
 };
 
 navno.contentLanguages = function () {
