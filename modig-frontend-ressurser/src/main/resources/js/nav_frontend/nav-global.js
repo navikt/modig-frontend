ddfd09197bcba14fe29e4b0d4f30d7b04ac73e2b
Ingrid Guren <ingrid.guren@nav.no>
2015-09-23 08:15:32 +0200

    Google analytics-kode

--- modig-frontend-ressurser/src/main/resources/js/nav_frontend/nav-global.js
+++ modig-frontend-ressurser/src/main/resources/js/nav_frontend/nav-global.js
@@ -733,6 +733,43 @@ $(function () {
 		});
 	}
 });
+
+
+
+var dataLayer = dataLayer || [{}];
+
+$(function() {
+
+	(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-PM9RP3');
+
+	$('a[data-ga]').on('click.google-analytics', function() {
+		var securityLevel = navno.securityLevel;
+		if (securityLevel === undefined) {
+			securityLevel = 0;
+		}
+
+		var fgKode = navno.fgkode;
+		var ytelse = navno.ytelse;
+		var eventAction = "klikk";
+		var eventLabel = $(this).text();
+		var eventCategory = $(this).attr('data-ga') + ": " + eventLabel;
+
+		var eventObj = {
+			'event': 'GAEvent',
+			'eventCategory': eventCategory,
+			'eventAction' : eventAction,
+			'eventLabel' : eventLabel,
+			'securityLevel' : securityLevel,
+			'fgKode' : fgKode,
+			'ytelse' : ytelse
+		};
+		dataLayer.push(eventObj);
+	});
+});
 //////////////////////// END ////////////////////////
 
 /*
