c22e616957bc7661c5d9cadbd4690f384fdb248a
Ingrid Guren <ingrid.guren@nav.no>
2015-10-12 14:02:00 +0200

    Innloggingsinformasjon i datalayer for Google Tag Manager

--- modig-frontend-ressurser/src/main/resources/js/nav_frontend/nav-global.js
+++ modig-frontend-ressurser/src/main/resources/js/nav_frontend/nav-global.js
@@ -750,36 +750,18 @@ $(function() {
 		'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 	})(window,document,'script','dataLayer','GTM-PM9RP3');
 
-	$('a[data-ga]').on('click.google-analytics', function() {
-		var securityLevel = navno.securityLevel;
-		if (securityLevel === undefined) {
-			securityLevel = 0;
-		}
+	var securityLevel = navno.securityLevel || 0;
+	var fgKode = navno.fgkode;
+	var ytelse = navno.ytelse;
 
-		var fgKode = navno.fgkode;
-		var ytelse = navno.ytelse;
-		var eventAction = "klikk";
+	var innloggetInformasjon = {
+		'securityLevel' : securityLevel,
+		'fgKode' : fgKode,
+		'ytelse' : ytelse
+	};
 
-		var explainingLabel = $(this).attr('class');
+	dataLayer.push(innloggetInformasjon);
 
-		if (explainingLabel === undefined || explainingLabel === "") {
-			explainingLabel = $(this).text();
-		}
-
-		var eventCategory = $(this).attr('data-ga');
-		var eventLabel = eventCategory + "/" + explainingLabel;
-
-		var eventObj = {
-			'event': 'GAEvent',
-			'eventCategory': eventCategory,
-			'eventAction' : eventAction,
-			'eventLabel' : eventLabel,
-			'securityLevel' : securityLevel,
-			'fgKode' : fgKode,
-			'ytelse' : ytelse
-		};
-		dataLayer.push(eventObj);
-	});
 });
 //////////////////////// END ////////////////////////
 
