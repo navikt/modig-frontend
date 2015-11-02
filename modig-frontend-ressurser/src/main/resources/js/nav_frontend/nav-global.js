73f278362f45f0941edaada9bdb1dcec6820d111
Ingrid Guren <ingrid.guren@nav.no>
2015-11-02 15:57:43 +0100

    Endre mellomrom mellom hjelpetekstboks og spørsmålstegn (påloggingsnivå)

--- modig-frontend-ressurser/src/main/resources/js/nav_frontend/nav-global.js
+++ modig-frontend-ressurser/src/main/resources/js/nav_frontend/nav-global.js
@@ -1050,7 +1050,7 @@ $(function() {
 		var tooltip = $("#globalmenu-upgrade-info-tooltip")[0];
 		var tooltipbuttonOffset = $("#globalmenu-upgrade-info-button").offset();
 		var leftPosition = tooltipbuttonOffset.left - $(tooltip).width() + 21;
-		var topPosition = tooltipbuttonOffset.top - $(tooltip).height() - 26;
+		var topPosition = tooltipbuttonOffset.top - $(tooltip).height() - 34;
 
 		$(tooltip).offset({
 			top: topPosition,
