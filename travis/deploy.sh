b5f4c27ef4df35cfcf1dbf7b0c266fda6861cf11
Eirik <eiriksletteberg@gmail.com>
2018-04-16 19:53:34 +0200

    Legg inn Travis CI-oppsett

---
+++ travis/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+if [ "$TRAVIS_BRANCH" = 'modig-frontend_3' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    openssl aes-256-cbc -K $encrypted_75b18248efca_key -iv $encrypted_75b18248efca_iv \
+  -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
+    gpg --fast-import travis/codesigning.asc
+    mvn --settings travis/settings.xml deploy -Prelease -DskipTests=true
+fi
