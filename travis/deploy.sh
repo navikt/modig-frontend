aca60a3b5df820e4e015f2cc3bc78acce93b0630
Eirik <eiriksletteberg@gmail.com>
2018-04-16 18:59:26 +0200

    Fiks syntaksfeil i bash-script

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 if [ "$TRAVIS_BRANCH" = 'modig-frontend_4' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_75b18248efca_key -iv $encrypted_75b18248efca_iv
+    openssl aes-256-cbc -K $encrypted_75b18248efca_key -iv $encrypted_75b18248efca_iv \
   -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
     gpg --fast-import travis/codesigning.asc
     mvn --settings travis/settings.xml deploy -Prelease -DskipTests=true
