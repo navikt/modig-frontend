acacd53d478c3158ee8dadd5d8e6bfb784712ec9
Eirik Sletteberg <eiriksletteberg@gmail.com>
2018-04-17 14:15:49 +0200

    Feil branch

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-if [ "$TRAVIS_BRANCH" = 'modig-frontend_3' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if [ "$TRAVIS_BRANCH" = 'modig-frontend_1' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_75b18248efca_key -iv $encrypted_75b18248efca_iv \
   -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
     gpg --fast-import travis/codesigning.asc
